0324eab5b9d27ec074648ea5585beff9735f95a6
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-17 16:15:28 -0500

    add react's state to all components

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,12 +1,20 @@
-import React from 'react'; 
+import React, { useState } from "react";
 
 function LightSwitchButton(){
+  const [light, setLight] = useState('off');
+
+
   return (
     <button className="LightSwitchButton">
-      {/* When the state is on */}<span className="on"><i>💡</i> I'm on!</span>
-      {/* When the state is off */}<span className="off"><i>💡</i> I'm off!</span>
+      {/* When the state is on */}
+      {light === "on" && <span className="on"><i>💡</i> I'm on!</span>}
+      {/* When the state is off */}
+      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
   );
 }
 
 export default LightSwitchButton;
+
+
+
